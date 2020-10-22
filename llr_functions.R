92353c84507e905fe40439fd1deaf10d727ff5de
auhappy11 <73006184+auhappy11@users.noreply.github.com>
2020-10-22 02:36:08 -0400

    Update llr_functions.R

--- llr_functions.R
+++ llr_functions.R
@@ -9,8 +9,6 @@ llr = function(x, y, z, omega) {
 compute_f_hat = function(z, x, y, omega) {
   Wz = diag(make_weight_matrix(z, x, omega))
   X = make_predictor_matrix(x)
-  #f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
-  #use sweep function and vectoriezed function
   f_hat = c(1, z) %*% solve(t(X) %*% sweep(X, 1, Wz, "*")) %*% t(X) %*% matrix(Wz * y)
   return(f_hat)
 }
